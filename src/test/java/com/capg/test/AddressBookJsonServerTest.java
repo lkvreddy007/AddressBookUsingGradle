4d386b3924180ddb64df62056c9fb18bb626b0e5
lkvreddy007 <lkvreddy007@gmail.com>
2020-11-10 01:34:22 +0530

    [Krishna]added-abilityToAddMultipleContactsToJsonServer

--- src/test/java/com/capg/test/AddressBookJsonServerTest.java
+++ src/test/java/com/capg/test/AddressBookJsonServerTest.java
@@ -1,14 +1,11 @@
 package com.capg.test;
 
-import static com.capg.EmployeePayrollService.IOService.REST_IO;
-
+import java.time.LocalDate;
 import java.util.Arrays;
 
 import org.junit.Before;
 import org.junit.Test;
 
-import com.capg.EmployeePayrollData;
-import com.capg.EmployeePayrollService;
 import com.capg.dto.AddressBookDataService;
 import com.capg.dto.AddressBookDataService.IOService;
 import com.capg.dto.Contact;
@@ -30,8 +27,8 @@ public class AddressBookJsonServerTest {
 	private Contact[] getContactList() {
 		Response response = RestAssured.get("/addressBook");
 		System.out.println("CONTACTS IN JSONServer:\n"+response.asString());
-		Contact[] arrayOfEmps = new Gson().fromJson(response.asString(),Contact[].class);
-		return arrayOfEmps;
+		Contact[] arrayOfContacts = new Gson().fromJson(response.asString(),Contact[].class);
+		return arrayOfContacts;
 	}
 	
 	private Response addContactToJsonServer(Contact contact) {
@@ -66,8 +63,33 @@ public class AddressBookJsonServerTest {
 		Contact[] arrayContacts = getContactList();
 		AddressBookDataService addressBookDataService;
 		addressBookDataService = new AddressBookDataService(Arrays.asList(arrayContacts));
-		long entries = addressBookDataService.countEntries(REST_IO);
+		long entries = addressBookDataService.countEntries(IOService.REST_IO);
 		Assert.assertEquals(2, entries);
+	}
+	
+	@Test
+	public void givenMultipleContacts_WhenAdded_ShouldMatch201ResponseCount() {
+		Contact[] arrContacts = getContactList();
+		AddressBookDataService addressBookDataService;
+		addressBookDataService = new AddressBookDataService(Arrays.asList(arrContacts));
+		
+		Contact[] arrayOfContacts = {
+				new Contact(0,"Rohith","MK", "Gajuvaka","Vizag","Andhra Pradesh","409002","9030303","jdksj@gmail.com"),
+				new Contact(0, "Vamsi","G","RS Colony","Guntur","Andhra Pradesh","408223","49949943","sjksjda@gmail.com"),
+				new Contact(0, "Anil", "M","New town","Mahabubnagar","Telangana","509001","3888393","dkakjsj@gmail.com"),
+		};
+		
+		for(Contact contact :arrayOfContacts) {
+			Response response = addContactToJsonServer(contact);
+			int statusCode = response.getStatusCode();
+			Assert.assertEquals(201, statusCode);
+			
+			contact = new Gson().fromJson(response.asString(), Contact.class);
+			addressBookDataService.addContactToAddressBook(contact, IOService.REST_IO);
+		}
+		
+		long entries = addressBookDataService.countEntries(IOService.REST_IO);
+		Assert.assertEquals(5, entries);
 	}
 	
 }
